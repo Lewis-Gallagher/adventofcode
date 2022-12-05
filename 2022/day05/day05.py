5af428a460a2282496566beb456cf32f106c5cc7
lgallagher <lewisjgallagher@gmail.com>
2022-12-05 23:44:46 +0000

    Day5 update

--- 2022/day05/day05.py
+++ 2022/day05/day05.py
@@ -1,15 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
 def get_data(file):
     with open(file) as f:
         data = [[i for i in line.splitlines()] for line in f.read().split('\n\n')]
-
+        
     return data
 
-
 def get_instructions(data):
-
+    
     stacks = data[0]
     moves = data[1]
-
+    
     # Clean stacks, assign Empty to an empty position and transpose it so each sub list is a column instead of a row
     stacks = [s.replace('[','').replace(']','').replace('    ',' Empty ').split() for s in stacks[:-1]]
     stacks = [list(l) for l in zip(*stacks)]
@@ -19,21 +24,20 @@ def get_instructions(data):
 
     # Take only every other element of the moves list to get only the numbers
     moves = [[int(i)-1 for i in move.split(' ')[1::2]] for move in moves]
-
+    
     return stacks, moves
 
 
 def crateMover9000(stacks, moves):
-    for quant, take, put in moves:
-        for i in range(quant+1):
-            stacks[put].insert(0, stacks[take].pop(0))
-
+    [[stacks[put].insert(0, stacks[take].pop(0)) for i in range(quant+1)] for quant, take, put in moves]
+        
     return ''.join([s[0] for s in stacks if s])
 
 
 def crateMover9001(stacks, moves):
     for quant, take, put in moves:
-        stacks[put] = stacks[take][:quant+1] + stacks[put]
+
+        stacks[put] = stacks[take][:quant+1] + stacks[put] 
         stacks[take] = stacks[take][quant+1:]
 
     return ''.join([s[0] for s in stacks if s])
@@ -47,3 +51,10 @@ print(part1)
 stacks, moves = get_instructions(data)
 part2 = crateMover9001(stacks, moves)
 print(part2)
+
+
+# In[ ]:
+
+
+
+
